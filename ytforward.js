0efbd45974f7ea9ec31f7bbc972f1018cde92ac9
Yuizz <julianglz@outlook.es>
2022-08-05 23:51:47 -0500

    Store actual speed

--- ytforward.js
+++ ytforward.js
@@ -1,16 +1,22 @@
 const video = document.getElementsByTagName("video")[0];
-const key = 17; //ctrl
+const selectedSpeed = document.getElementById("selected-speed");
+const key = "Control";
 const bulletSpeed = 4;
 
+const speedStore = document.createElement("input");
+speedStore.hidden = true;
+speedStore.id = "speed-store";
+speedStore.value = video.playbackRate;
+document.body.appendChild(speedStore);
+
 window.addEventListener("keydown", (event) => {
-  if (event.isComposing || event.keyCode === key) {
-    if (video.playbackRate !== bulletSpeed) {
-      video.playbackRate = bulletSpeed;
-    }
+  if ((event.isComposing || event.key === key) && !event.repeat) {
+    speedStore.value = video.playbackRate;
+    video.playbackRate = bulletSpeed;
   }
 });
 window.addEventListener("keyup", (event) => {
-  if (event.isComposing || event.keyCode === key) {
-    video.playbackRate = 1; //normal
+  if (event.isComposing || event.key === key) {
+    video.playbackRate = speedStore.value; //normal
   }
 });
